dc85cc75a29c318f29cb05878b64a73227f5f3b8
Kayki Macedo <71573899+Macedo-Kayki@users.noreply.github.com>
2025-08-28 21:02:22 -0300

    Aula 01

---
+++ kayki_python01/exibicao/main.py
@@ -0,0 +1,16 @@
+print("\n"+"="*18+"Dados Pessoais"+"="*18)
+name = input("Digite seu nome: ")
+idade = int(input("Digite a sua idade: "))
+renda = float(input("Digite sua renda: "))
+course = input("Digite o seu curso: ")
+print("="*18+"Dados Pessoais"+"="*18+"\n")
+print("="*17+"Dados de Contato"+"="*17)
+email = input("Digite o seu E-mail: ")
+cell = input("Digite o número do seu celular com o DDD: ")
+print("="*17+"Dados de Contato"+"="*17)
+
+print("\n"+"="*18+"DADOS PESSOAIS"+"="*18+f"\nNome: {name}\n"+"="*50+f"\nIdade: {idade}\n"+"="*50+f"\nRenda: R$ {renda:2f}\n"+"="*50+f"\nCurso: {course}\n"
+      +"="*18+"DADOS PESSOAIS"+"="*18+"\n\n"+"="*17+"DADOS DE CONTATO"+"="*17+f"\nEmail: {email}\n"+"="*50+f"\nCelular: {cell}\n"
+      +"="*17+"DADOS DE CONTATO"+"="*17)
+
+print("="*20+"Dados pessoais"+"="*20)
---
+++ kayki_python01/media/main.py
@@ -0,0 +1,7 @@
+n1 = float(input("\nDigite o 1º número: "))
+n2 = float(input("Digite o 2º número: "))
+n3 = float(input("Digite o 3º número: "))
+
+media = (n1+n2+n3)/3
+
+print(f"\nA média dos números:{n1}, {n2} e {n3} é igual à {media:.2f}\n")

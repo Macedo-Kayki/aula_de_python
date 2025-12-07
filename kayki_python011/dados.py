fc22f8f62ec42ed34b5be5d9eaae2ce79598f103
Kayki Macedo <71573899+Macedo-Kayki@users.noreply.github.com>
2025-12-06 21:57:59 -0300

    Update dados.py

--- kayki_python011/dados.py
+++ kayki_python011/dados.py
@@ -1,10 +1,29 @@
-from models import Pedido
+from models import Produto, Pedido
 
 def criar_pedidos_exemplo():
-    Pedido(1, ["banana", 3.90])
-    Pedido(2, ["banana", 3.90])
-    Pedido(3, ["maçã", 8.90])
-    Pedido(4, ["maracuja", 5.90])
-    Pedido(5, ["mamão", 8.90])
-    Pedido(6, ["leite", 6.90])
-    Pedido(7, ["mamão", 8.90])
+    
+    p1 = Produto("Lasanha", 45.0)
+    p2 = Produto("Refrigerante", 8.0)
+    p3 = Produto("Pudim", 12.0)
+    p4 = Produto("Salada", 25.0)
+    p5 = Produto("Cafe", 5.0)
+
+    pedidos = []
+
+    ped1 = Pedido()
+    ped1.adicionar_produto(p1)
+    ped1.adicionar_produto(p2)
+    pedidos.append(ped1)
+
+    ped2 = Pedido()
+    ped2.adicionar_produto(p4)
+    ped2.adicionar_produto(p2)
+    pedidos.append(ped2)
+
+    ped3 = Pedido()
+    ped3.adicionar_produto(p1)
+    ped3.adicionar_produto(p3)
+    ped3.adicionar_produto(p5)
+    pedidos.append(ped3)
+
+    return pedidos
